725eae95002ca68051476127014d60b1c2bad1f6
markusthilo <markus.thilo@gmail.com>
2024-01-25 11:03:01 +0100

    Working on make-exe.py

--- make-exe.py
+++ make-exe.py
@@ -14,41 +14,40 @@ from pathlib import Path
 from shutil import rmtree
 from sys import argv
 from argparse import ArgumentParser
-import PyInstaller
+import PyInstaller.__main__
 
 
-class MkExe(PyInstaller):
-	'''PyInstaller for FallbackImager'''
+class MkExe:
+	'''Use PyInstaller to build Windows executable'''
 
 	def __init__(self, pyfile, noconsole=False, admin=False):
 		'''Build standalone executable'''
-		self.py_path = Path(pyfile)
+		self.py_path = Path.cwd()/pyfile
 		args = [pyfile, '--onefile', '--icon', 'appicon.ico']
 		if noconsole:
 			args.append('--noconsole')
 		if admin:
 			args.append('--uac-admin')
-		print(args)
-		#super().__main__.run(args)
-		self.exe_path = (Path(pyfile)/'dist').with_suffix('.exe')
+		PyInstaller.__main__.run(args)
+		self.exe_path = Path.cwd()/'dist'/f'{self.py_path.stem}.exe'
 
-	def move(self, destination):
+	def move(self, tobin=False):
 		'''Move executable to destination directory and clean up'''
-		if not destination:
-			destination = Path.cwd()
-		else:
-			destination = Path(destination)
-		self.exe_path = self.exe_path.rename(destination/self.exe_path.name)
-		#rmtree('build')
-		#rmtree('dist')
-		#self.py_path.with_suffix('.spec').unlink()
+		dir_path = Path.cwd()
+		if tobin:
+			dir_path = dir_path/'bin'
+		print(self.exe_path, dir_path, dir_path/self.exe_path.name)
+		self.exe_path = self.exe_path.rename(dir_path/self.exe_path.name)
+		rmtree('build')
+		rmtree('dist')
+		self.py_path.with_suffix('.spec').unlink()
 
 if __name__ == '__main__':	# start here if called as application
 	argparser = ArgumentParser(description=__description__)
-	self.add_argument('-c', '--cli', action='store_true', help='CLI tools')
-	self.add_argument('-g', '--gui', action='store_true', help='GUI tool FallbackImager')
-	self.add_argument('-w', '--wim', action='store_true', help='GUI tool WimMount')
-	self.add_argument('additional', nargs='?', type=Path,
+	argparser.add_argument('-c', '--cli', action='store_true', help='CLI tools')
+	argparser.add_argument('-g', '--gui', action='store_true', help='GUI tool FallbackImager')
+	argparser.add_argument('-w', '--wim', action='store_true', help='GUI tool WimMount')
+	argparser.add_argument('additional', nargs='?', type=Path,
 		help='Additional Python files to compile', metavar='FILE'
 	)
 	args = argparser.parse_args()
@@ -62,6 +61,6 @@ if __name__ == '__main__':	# start here if called as application
 	if args.gui:
 		MkExe('FallbackImager.py', admin=True, noconsole=True).move()
 	if args.wim or len(argv) == 1:
-		MkExe('WimMount.py', admin=True, noconsole=True).move('bin')
+		MkExe('WimMount.py', admin=True, noconsole=True).move(tobin=True)
 
 
