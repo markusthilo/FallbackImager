a1848b5a0ba887287913467b897f365595173997
Markus Thilo <markus.thilo@gmail.com>
2024-03-11 14:57:09 +0000

    Working on GUI

--- testgui.py
+++ testgui.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from tkinter import Tk, Canvas, Scrollbar
-from tkinter.ttk import Frame
+from tkinter.ttk import Frame, Label
 
-class ScrollFrame
-'''Frame with scroll bars and buttons'''
+class ScrollFrame:
+	'''Frame with scroll bars and buttons'''
 
 	def __init__(self, root):
 		'''Build the frame'''
@@ -22,40 +22,22 @@ class ScrollFrame
 		self.canvas.pack(fill='both', side='left', expand=True)
 		self.canvas.create_window(0, 0, window=self.scrolled_frame, anchor='nw')
 
-		for i in range(10)
+		self.scrolled_frame.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
+		for i in range(10):
 			Label(self.scrolled_frame, text="Hello World").grid(row=i, column=i)
-	i+=1
-
-	# Update the scroll region after new widgets are added
-	updateScrollRegion()
-
-	root.after(1000, addNewLabel)
-
-	def _update_canvas():
-		'''Updates the scrollable region of the canvas'''
-		self.canvas.update_idletasks()
-		self.canvas.config(scrollregion=fTable.bbox())
 
 
-# Adds labels diagonally across the screen to demonstrate the scrollbar adapting to the increasing size
-i=0
-def addNewLabel():
-	global i
-	tk.Label(fTable, text="Hello World").grid(row=i, column=i)
-	i+=1
 
-	# Update the scroll region after new widgets are added
-	updateScrollRegion()
+		#self.canvas.update_idletasks()
+		#self.canvas.config(scrollregion=self.canvas.bbox('all'))
 
-	root.after(1000, addNewLabel)
 
-createScrollableContainer()
-addNewLabel()
 
-root.mainloop()
+		#updateScrollRegion()
 
 
 if __name__ == '__main__':	# start here if called as application
 	root = Tk()
-	
+	ScrollFrame(root)
 	root.mainloop()
