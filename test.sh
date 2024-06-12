205fe9a2ab6ed9547b587f9b88c78711da85a1fb
Markus Thilo <markus.thilo@gmail.com>
2024-06-12 10:14:06 +0000

    V0.5.2

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-
-rootuuid=`cat /mnt/archimager/etc/fstab \
-| grep '/efi' | grep -o 'UUID=[^ ]*'`
-liminecfg='TIMEOUT=5\n\n:ArchImager\n    PROTOCOL=linux\n    KERNEL_PATH=boot:///vmlinuz-linux\n'
-liminecfg+="    CMDLINE=root=${rootuuid} rw\n    MODULE_PATH=boot:///initramfs-linux.img\n"
-echo -e "${liminecfg}" | sudo tee /mnt/archimager/boot/limine.cfg
