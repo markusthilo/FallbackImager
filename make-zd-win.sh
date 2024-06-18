dd74411189e3281492dff327e7b709499a52a486
Markus Thilo <markus.thilo@gmail.com>
2024-06-18 10:31:46 +0200

    v0.5.2

--- make-zd-win.sh
+++ make-zd-win.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-mkdir -pv dist-win/bin
+mkdir -pv dist-win
 echo 'Compiling c/zd-win.c to dist-win/bin/zd-win.exe'
-x86_64-w64-mingw32-gcc -o dist-win/bin/zd-win.exe c/zd-win.c
+x86_64-w64-mingw32-gcc -o dist-win/zd-win.exe c/zd-win.c
